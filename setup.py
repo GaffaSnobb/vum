b2e2149c5e37d50869a209691d8111cc3ad4e4d4
Jon Dahl <jon@dira.no>
2023-07-14 17:28:54 +0900

    Update description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'vum',
     version = '1.0.0.0',    
-    description = 'Handy utilities for handling nuclear shell model calculations from KSHELL',
+    description = 'A simple UI with curses',
     # long_description = long_description,
     url = 'https://github.com/GaffaSnobb/vum',
     author = ['Jon Kristian Dahl',],
@@ -15,6 +15,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Operating System :: MacOS',
         'Operating System :: Unix',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
