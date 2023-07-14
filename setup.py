91c79ed07699631bd0f9ba68ed48d8ee128c4e2c
Jon Dahl <jon@dira.no>
2023-07-14 17:59:34 +0900

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'vum',
-    version = '1.0.0.0',    
+    version = '1.0.0.1',    
     description = 'A simple UI with curses',
     # long_description = long_description,
     url = 'https://github.com/GaffaSnobb/vum',
